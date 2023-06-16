c91377341afa4ea9543b563f367a89074f65c110
Qwertyadmin <Qwertyadmin@users.noreply.github.com>
2023-06-16 16:16:50 +0200

    Implemented requested changes in request #6.

---
+++ src/models/interfaces/users.ts
@@ -0,0 +1,6 @@
+export interface IUser {
+    username: String,
+    email: String,
+    bio: String,
+    avatar?: String,
+}
--- src/models/users.ts
+++ src/models/users.ts
@@ -1,18 +1,13 @@
 import { Model, Schema, model } from "mongoose";
+import { IUser } from "./interfaces/users";
 
-export interface IUSer {
-    username: String,
-    email: String,
-    bio: String,
-    avatar?: String,
-}
-type UserModelType = Model<IUSer>;
+type UserModelType = Model<IUser>;
 
-const userSchema = new Schema<IUSer, UserModelType>({
+const userSchema = new Schema<IUser, UserModelType>({
     username: {type: String, required: true},
     email:  {type: String, required: true},
     bio:  {type: String, required: true},
     avatar:  {type: String},
 });
 
-export const User = model<IUSer>("User", userSchema);
+export const User = model<IUser>("User", userSchema);
